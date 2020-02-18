b2c6d86dc99b59cc8880cacd762c4dfcbc049d06
Simone Sala <simonesala@me.com>
2020-02-18 16:34:21 +0000

    Consegna

--- js/script.js
+++ js/script.js
@@ -8,16 +8,21 @@
 var km = prompt("Quanti km vuoi fare?");
 var age = prompt("Inserisci la tua età.");
 
-var  costoKm = 0.21;
+var costoKm = 0.21;
 var prezzoPieno = 0.21 * km;
 var prezzoFinale;
 
 if (age > 65){
     prezzoFinale = prezzoPieno * 0.6;
+    document.getElementById("sconto-over").setAttribute("class","visible");
 } else if (age < 18){
     prezzoFinale = prezzoPieno * 0.8;
+    document.getElementById("sconto-giovani").setAttribute("class","visible");
 } else {
     prezzoFinale = prezzoPieno;
 }
 
-console.log(prezzoFinale);
+document.getElementById("eta").innerHTML = age;
+document.getElementById('percorso').innerHTML = km;
+document.getElementById('spesa').innerHTML = prezzoFinale;
+document.getElementById("container").setAttribute("class","visible");
